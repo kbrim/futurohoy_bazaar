68ec8bd85654e4b4171cf322a140d7483a787ffa
Fabián González Salas <fgonz072@codelco.cl>
2024-11-05 01:29:42 -0300

    ci: using standalone

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: { theme: "DEFAULT", currency: "USD" },
+  output: 'standalone',
+  productionBrowserSourceMaps: false,
   publicRuntimeConfig: { theme: "DEFAULT", currency: "USD" },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "ui-lib.com" }],
